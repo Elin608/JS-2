38b33cc9db3fecceeefade498fa2de869bf39e5a
Elin <elinhedinlarsson3@gmail.com>
2026-04-13 10:59:21 +0200

    add movies array

---
+++ Script/assignment6.js
@@ -0,0 +1,20 @@
+let movies=[];
+movies.push({
+    title:("Frozen"),
+    year:2013,
+    isWatched:true
+
+});
+movies.push({
+    title:("Lion King"),
+    year:2000,
+    isWatched:false
+
+});
+movies.push({
+    title:("Aladdin"),
+    year:2025,
+    isWatched:true
+
+});
+console.log(movies);
