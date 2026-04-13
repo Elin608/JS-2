9270990efcf9c77ab9b8444b3ecc7b4db1db7261
Elin <elinhedinlarsson3@gmail.com>
2026-04-13 12:10:01 +0200

    completed movie tracker

--- Script/assignment6.js
+++ Script/assignment6.js
@@ -37,8 +37,23 @@ function addMovie(){
 
 function listMovies(){
     movies.forEach(function(movie){
-        console.log(movie.title + "("+movie.year+")");
+       console.log(movie.title + "("+movie.year+")");
 
         });
     }
 listMovies();
+
+function markAsWatched(title){
+    let movie = movies.find(function(movie){
+       return movie.title ===title;
+    });
+   if(movie){
+        movie.isWatched = true;
+       console.log("Movie watched");
+       }else{
+            console.log("Movie not found");
+        }
+   }
+    markAsWatched("Frozen");
+    console.log(movies)
+//addMovie();
